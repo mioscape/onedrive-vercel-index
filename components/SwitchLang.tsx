fc37210d4c95625549aa30487e7addf7a7d1e978
ihya.dev <git@ihya.dev>
2022-10-12 13:06:48 +0700

    [Additional] Fix missing switcher
    Signed-off-by: ihya.dev <git@ihya.dev>

--- components/SwitchLang.tsx
+++ components/SwitchLang.tsx
@@ -19,6 +19,8 @@ const localeText = (locale: string): string => {
   switch (locale) {
     case 'en':
       return '🇬🇧 English'
+    case 'id':
+      return '🇮🇩 Bahasa Indonesia'
     case 'zh-CN':
       return '🇨🇳 简体中文'
     case 'hi':
